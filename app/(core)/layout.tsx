fa22fe6e151dd6fd5f28c7cd3119bdb67127a470
Bruno Lustro <brunolustro@gmail.com>
2026-06-12 14:05:17 -0300

    Move layout and page to core folder
    dynamic core folder has been created so it does not conflict with original layout file

---
+++ app/(core)/layout.tsx
@@ -0,0 +1,18 @@
+import { LeftSidebar } from '@/components/ui/layout/left-sidebar';
+import { Navbar } from '@/components/ui/layout/navbar';
+
+export default async function CoreGroupLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <Navbar />
+      <div className='mx-auto flex max-w-300 gap-8 px-4 pb-16 pt-2'>
+        <LeftSidebar />
+        <div className='min-w-0 flex-1'>{children}</div>
+      </div>
+    </>
+  );
+}
